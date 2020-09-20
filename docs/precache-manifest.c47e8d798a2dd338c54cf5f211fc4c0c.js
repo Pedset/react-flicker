8def01ac69918b0e6de8a7db6ccec73bcacf6145
Pedset <57757244+Pedset@users.noreply.github.com>
2020-09-20 21:53:41 +0200

    v2

---
+++ docs/precache-manifest.c47e8d798a2dd338c54cf5f211fc4c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936c9740b86812cf0c894957a13b3477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164dbbcc2ad4ba27e189",
+    "url": "/static/css/main.061a251a.chunk.css"
+  },
+  {
+    "revision": "b8df94a4b5b70ca2d181",
+    "url": "/static/js/2.60ed789b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60ed789b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164dbbcc2ad4ba27e189",
+    "url": "/static/js/main.dfd683c3.chunk.js"
+  },
+  {
+    "revision": "8bee09bf674b1bc63318",
+    "url": "/static/js/runtime-main.39d693e7.js"
+  }
+]);
